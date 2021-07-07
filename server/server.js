456191c57c256ca50a942e6d1d4b0a16cc7c6ddd
martinivnv <martinivnv2002@gmail.com>
2021-07-07 14:31:00 -0400

    Setup Express server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+	// perform a database connection when server starts
+	dbo.connectToServer(function (err) {
+		if (err) console.error(err);
+	});
+	console.log(`Server is running on port: ${port}`);
+});
