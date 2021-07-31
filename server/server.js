e6edff74dc1bb586b52f7426897a7c5257ef5a85
martinivnv <martinivnv2002@gmail.com>
2021-07-31 14:06:14 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// get driver connection
-const dbo = require("./db/conn");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+	console.log("Connection to MongoDB established successfully!");
+});
+
+const usersRouter = require("./routes/users");
+
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
-	// perform a database connection when server starts
-	dbo.connectToServer(function (err) {
-		if (err) console.error(err);
-	});
 	console.log(`Server is running on port: ${port}`);
 });
